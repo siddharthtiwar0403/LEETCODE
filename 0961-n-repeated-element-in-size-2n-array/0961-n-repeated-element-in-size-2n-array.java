c3659462fe37249641baf66f50ba0f430b8ae56f
Siddharth Tiwari <150476061+siddharthtiwar0403@users.noreply.github.com>
2026-01-02 13:45:28 +0530

    Time: 0 ms (100%), Space: 47.4 MB (65.18%) - LeetHub

---
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int repeatedNTimes(int[] nums) {
+        if(nums[0] == nums[nums.length - 1]) return nums[0];
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == nums[i + 1] || nums[i] == nums[i + 2]) return nums[i];
+        }
+        return nums[0];
+    }
+}
